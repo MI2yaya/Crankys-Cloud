94ca2ab24eee732ea339aa7a2697b37eb2ee750d
MrDulfin <mrdulfin@dangoware.org>
2025-08-29 00:05:14 -0400

    Delete endpoint works.

--- src/pages/track/index.ts
+++ src/pages/track/index.ts
@@ -2,12 +2,12 @@ class DeleteSection extends HTMLElement {
   constructor() {
     super()
 
+    console.log(this.dataset.id);
     const button: HTMLElement = document.querySelector("[data-delete]")!;
     button.addEventListener("click", async () => {
       await fetch("/api/delete", {
         headers: {
-          session: JSON.stringify(this.dataset.session),
-          trackID: this.dataset.trackID as string
+          trackID: this.dataset.id as string,
         },
         method: "DELETE",
       }).then(console.log)
