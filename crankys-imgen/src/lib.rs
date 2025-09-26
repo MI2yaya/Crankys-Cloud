1af547abe78488e1388b085cd2529b0d861d67b5
MrDulfin <mrdulfin@dangoware.org>
2025-09-26 01:03:58 -0400

    added proper error handling to imgen worker

--- crankys-imgen/src/lib.rs
+++ crankys-imgen/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use http_body_util::BodyExt;
-use image::{EncodableLayout, ImageReader};
+use image::ImageReader;
 use serde::Deserialize;
 use worker::*;
 
@@ -38,38 +38,53 @@ struct Colour {
 
 #[event(fetch)]
 async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> Result<Response> {
-    dbg!(&req);
     let (parts, body) = req.into_parts();
-    let colours: HashMap<Channel, Colour> =
-        serde_json::from_str(parts.headers.get("colours").unwrap().to_str().unwrap()).unwrap();
+    let colours: HashMap<Channel, Colour> = if let Some(colour_map) = parts.headers.get("colours") {
+        match serde_json::from_str(colour_map.to_str().unwrap()) {
+            Ok(map) => map,
+            Err(e) => {
+                return Response::error(format!("Error parsing header \"colours\": {e:?}"), 400)
+            }
+        }
+    } else {
+        return Response::error("Header \"colours\" is missing", 400);
+    };
 
-    let mut image = ImageReader::new(std::io::Cursor::new(
-        body.collect().await.unwrap().to_bytes().as_bytes(),
-    ))
-    .with_guessed_format()?
-    .decode()
-    .unwrap()
-    .into_rgb8();
-    image.pixels_mut().for_each(|px| {
+    let cursor = std::io::Cursor::new(match body.collect().await {
+        Ok(body) => body.to_bytes(),
+        Err(e) => return Response::error(format!("failed to collect body: {e:?}"), 400),
+    });
+
+    let mut image = match ImageReader::new(cursor).with_guessed_format()?.decode() {
+        Ok(decoded) => decoded.into_rgb8(),
+        Err(e) => return Response::error(format!("Error decoding image: {e:?}"), 500),
+    };
+
+    for px in image.pixels_mut() {
         use Channel::*;
-        let get = match px.0 {
+        let channel = match px.0 {
             RED => RedChannel,
             BLUE => BlueChannel,
             GREEN => GreenChannel,
             YELLOW => YellowChannel,
             MAGENTA => MagentaChannel,
             CYAN => CyanChannel,
-            WHITE | BLACK => return,
-            c => panic!("unknown RBG Value {c:?} found"),
+            WHITE | BLACK => continue,
+            c => return Response::error(format!("unknown RBG Value {c:?} found"), 400),
         };
 
-        let colour = colours
-            .get(&get)
-            .expect("No colour map found for channel {channel:?}");
+        let colour = if let Some(colour) = colours.get(&channel) {
+            colour
+        } else {
+            return Response::error(format!("No colour map found for channel {channel:?}"), 400);
+        };
         *px = image::Rgb([colour.r, colour.g, colour.b]);
-    });
+    }
 
     let mut vec = std::io::Cursor::new(vec![]);
-    image.write_to(&mut vec, image::ImageFormat::Png).unwrap();
-    Response::from_bytes(vec.into_inner())
+    if let Err(e) = image.write_to(&mut vec, image::ImageFormat::Png) {
+        Response::error(format!("Error writing image to bufffer: {e:?}"), 500)
+    } else {
+        Response::from_bytes(vec.into_inner())
+    }
 }
